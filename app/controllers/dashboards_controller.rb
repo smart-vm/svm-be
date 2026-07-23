a2b34b4fce2b9e321b9fee751414a613ea7d18d6
sulsul2 <altaalvarojoe@gmail.com>
2026-07-23 14:45:42 +0700

    fix offense by rubocop

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,7 +5,7 @@ class DashboardsController < ApplicationController
     @total_machines = Machine.count
     @items_sold_today = Transaction.settled.where(created_at: today).count
     @today_revenue = Transaction.settled.where(created_at: today).sum(:amount)
-    @low_stock_count = Slot.where('current_stock < ?', 5).count
+    @low_stock_count = Slot.where("current_stock < ?", 5).count
 
     @recent_transactions = Transaction.includes(:machine, slot: :product).order(created_at: :desc).limit(4)
     @machines_list = Machine.order(created_at: :desc).limit(4)
