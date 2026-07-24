723eb0e8cc519d81aa09e4155be7617d3e0a8e57
sulsul2 <altaalvarojoe@gmail.com>
2026-07-24 13:21:34 +0700

    feat: implemented add and edit on machine

--- app/controllers/machines_controller.rb
+++ app/controllers/machines_controller.rb
@@ -1,5 +1,43 @@
 class MachinesController < ApplicationController
+  before_action :set_machine, only: [ :edit, :update ]
+
   def index
     @machines = Machine.includes(:slots).order(created_at: :desc)
+  end
+
+  def new
+    @machine = Machine.new
+  end
+
+  def create
+    @machine = Machine.new(machine_params)
+
+    if @machine.save
+      redirect_to machines_path, notice: "Machine successfully deployed at #{@machine.location_name}."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @machine.update(machine_params)
+      redirect_to machines_path, notice: "Machine at #{@machine.location_name} was updated."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_machine
+    @machine = Machine.find(params[:id])
+  end
+
+  def machine_params
+    # Only allow users to submit these specific fields
+    params.require(:machine).permit(:location_name, :status)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   root "dashboards#index"
 
-  resources :machines, only: [ :index ]
+  resources :machines, only: [ :index, :new, :create, :edit, :update ]
   resources :inventories, only: [ :index ]
-  resources :transactions, only: [:index]
+  resources :transactions, only: [ :index ]
 
   namespace :api do
     namespace :v1 do
-      # Gateway Webhook Route
       post 'payments/webhook', to: 'payments#webhook'
     end
   end
 
-  # Mount the live ActionCable server websocket route
   mount ActionCable.server => '/cable'
 end
